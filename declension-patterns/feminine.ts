8d5fffcc689150346fe7b6a691ab8e2b35f92f2e
bgub <benjamin.gubler@missionary.org>
2022-06-02 12:08:36 +0200

    All feminine and masculine cases

--- declension-patterns/feminine.ts
+++ declension-patterns/feminine.ts
@@ -21,3 +21,71 @@ export var žena = createDeclension(
   'woman',
   'women'
 );
+
+export var růže = createDeclension(
+  'f',
+  [
+    'růže',
+    'růže',
+    'růži',
+    'růži',
+    'růže!',
+    'růži',
+    'růží',
+    'růže',
+    'růží',
+    'růžím',
+    'růže',
+    'růže!',
+    'růžích',
+    'růžemi/ema',
+  ],
+  'rose',
+  'roses'
+);
+
+// add ulice
+
+export var píseň = createDeclension(
+  'f',
+  [
+    'píseň',
+    'písně',
+    'písni',
+    'píseň',
+    'písni!',
+    'písni',
+    'písní',
+    'písně',
+    'písní',
+    'písním',
+    'písně',
+    'písně!',
+    'písních',
+    'písněmi/ěma',
+  ],
+  'song',
+  'songs'
+);
+
+export var kost = createDeclension(
+  'f',
+  [
+    'kost',
+    'kosti',
+    'kosti',
+    'kost',
+    'kosti!',
+    'kosti',
+    'kostí',
+    'kosti',
+    'kostí',
+    'kostem',
+    'kosti',
+    'kosti!',
+    'kostech',
+    'kostmi/ma',
+  ],
+  'bone',
+  'bones'
+);
--- declension-patterns/masculine.ts
+++ declension-patterns/masculine.ts
@@ -1,6 +1,50 @@
 import { createDeclension } from '../utilities';
 
-var muž = createDeclension(
+export let pán = createDeclension(
+  'ma',
+  [
+    'pán',
+    'pána',
+    'pánu/ovi',
+    'pána',
+    'páne!',
+    'pánu/ovi',
+    'pánem',
+    'páni/ové',
+    'pánů',
+    'pánům',
+    'pány',
+    'páni/ové!',
+    'pánech',
+    'pány/ama',
+  ],
+  'lord',
+  'lords'
+);
+
+export let hrad = createDeclension(
+  'mi',
+  [
+    'hrad',
+    'hradu',
+    'hradu',
+    'hrad',
+    'hrade!',
+    'hradě/u',
+    'hradem',
+    'hrady',
+    'hradů',
+    'hradům',
+    'hrady',
+    'hrady!',
+    'hradech',
+    'hrady/ama',
+  ],
+  'castle',
+  'castles'
+);
+
+export let muž = createDeclension(
   'ma',
   [
     'muž',
@@ -22,4 +66,68 @@ var muž = createDeclension(
   'men'
 );
 
-export { muž };
+export let soudce = createDeclension(
+  'ma',
+  [
+    'soudce',
+    'soudce',
+    'soudci/ovi',
+    'soudce',
+    'soudce!',
+    'soudci/ovi',
+    'soudcem',
+    'soudci/ové',
+    'soudců',
+    'soudcům',
+    'soudce',
+    'soudci/ovi!',
+    'soudcích',
+    'soudci/ema',
+  ],
+  'man',
+  'men'
+);
+
+export let stroj = createDeclension(
+  'mi',
+  [
+    'stroj',
+    'stroje',
+    'stroji',
+    'stroj',
+    'stroji!',
+    'stroji',
+    'strojem',
+    'stroje',
+    'strojů',
+    'strojům',
+    'stroje',
+    'stroje!',
+    'strojích',
+    'stroji/ema',
+  ],
+  'machine',
+  'machines'
+);
+
+export let předseda = createDeclension(
+  'ma',
+  [
+    'předseda',
+    'předsedy',
+    'předsedovi',
+    'předsedu',
+    'předsedo!',
+    'předsedovi',
+    'předsedou',
+    'předsedové',
+    'předsedů',
+    'předsedům',
+    'předsedy',
+    'předsedové!',
+    'předsedech',
+    'předsedy/ama',
+  ],
+  'chairman',
+  'chairmen'
+);
