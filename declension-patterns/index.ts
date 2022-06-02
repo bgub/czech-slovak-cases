94dc593a1b7801bd9ca054eb0a4ccf37ceeef3f5
bgub <benjamin.gubler@missionary.org>
2022-06-02 12:29:14 +0200

    All declensions, nice exports

--- declension-patterns/index.ts
+++ declension-patterns/index.ts
@@ -1,91 +1,16 @@
-import { žena } from './feminine';
-import { muž } from './masculine';
+import { pán, hrad, muž, soudce, stroj, předseda } from './masculine';
+import { město, vízum, moře, stavení, kuře } from './neuter';
+import { žena, růže, píseň, kost } from './feminine';
 import { prepositions } from '../prepositions';
 import { první, malý } from './adjectives';
-import { to as toDec, všechno } from './demonstratives';
+import { to, všechno } from './demonstratives';
 import { nounDeclension, declensionArray, gender } from '../types';
 
-let declensionList = ['1', '2', '3', '4', '4-m', '5', '6', '7'];
+export let masculineNouns = [pán, hrad, muž, soudce, stroj, předseda];
+export let neuterNouns = [město, vízum, moře, stavení, kuře];
+export let feminineNouns = [žena, růže, píseň, kost];
+export let allNouns = masculineNouns.concat(neuterNouns).concat(feminineNouns);
 
-function getDefaultPreposition(desiredCase: string) {
-  return prepositions.find(function (prep) {
-    if (prep.case === desiredCase && prep.default) {
-      return true;
-    }
-  });
-}
+export let basicAdjectives = [první, malý];
 
-let gList = {
-  ma: 0,
-  mi: 1,
-  n: 2,
-  f: 3,
-};
-
-function getItem(
-  number: number,
-  gender: gender,
-  noun: declensionArray,
-  plural: boolean
-) {
-  if (plural) {
-    number += 7;
-  }
-
-  console.log(number);
-  console.log(noun);
-
-  let dec = noun[number - 1];
-  console.log(dec);
-
-  if (typeof dec === 'string') {
-    return dec;
-  } else {
-    return dec[gList[gender]];
-  }
-}
-
-let getTo = (noun: nounDeclension, number: number, plural: boolean) =>
-  getItem(number, noun.gender, toDec, plural);
-
-let getPrvní = (noun: nounDeclension, number: number, plural: boolean) =>
-  getItem(number, noun.gender, první, plural);
-
-let getNoun = (noun: nounDeclension, number: number, plural: boolean) =>
-  getItem(number, noun.gender, noun.caseArray, plural);
-
-export function generateSimpleSentences(noun: nounDeclension) {
-  let res = [];
-
-  for (var i in declensionList) {
-    let declensionName = declensionList[i];
-    let declensionNumberString = declensionName.replace(/[^1234567]/g, '');
-    let declensionNumber = Number(declensionNumberString);
-    console.log('declensionNumberString ' + declensionNumberString);
-    console.log('declensionNumber ' + declensionNumber);
-
-    let tempEnglishRes = '';
-    let tempCzechRes = '';
-
-    // if (i !== '1' && i !== '5') {
-    if (true) {
-      tempEnglishRes =
-        // getDefaultPreposition(declensionList[i]).preposition ||
-        // 'foo' +
-        // ' ' +
-        getTo(noun, declensionNumber, false) +
-        ' ' +
-        getPrvní(noun, declensionNumber, false) +
-        ' ' +
-        getNoun(noun, declensionNumber, false);
-
-      tempCzechRes =
-        //getDefaultPreposition(declensionList[i]).english +
-        ' ' + 'that' + ' ' + 'first' + ' ' + noun.objectiveCase;
-    }
-
-    res.push(tempEnglishRes + ' - ' + tempCzechRes);
-  }
-
-  return res;
-}
+export { prepositions, to, všechno };
---
+++ declension-patterns/neuter.ts
@@ -0,0 +1,111 @@
+import { createDeclension } from '../utilities';
+
+export let město = createDeclension(
+  'n',
+  [
+    'město',
+    'města',
+    'městu',
+    'město',
+    'město!',
+    'městě/u',
+    'městem',
+    'města',
+    'měst',
+    'městům',
+    'města',
+    'města!',
+    'městech',
+    'městy/ama',
+  ],
+  'city',
+  'cities'
+);
+
+export let vízum = createDeclension(
+  'n',
+  [
+    'vízum',
+    'víza',
+    'vízu',
+    'vízum',
+    'vízum!',
+    'vízu',
+    'vízem',
+    'víza',
+    'víz',
+    'vízům',
+    'víza',
+    'víza!',
+    'vízech',
+    'vízy/ama',
+  ],
+  'visa',
+  'visas'
+);
+
+export let moře = createDeclension(
+  'n',
+  [
+    'moře',
+    'moře',
+    'moři',
+    'moře',
+    'moře!',
+    'moři',
+    'mořem',
+    'moře',
+    'moří',
+    'mořím',
+    'moře',
+    'moře!',
+    'mořích',
+    'moři/ema',
+  ],
+  'sea',
+  'seas'
+);
+
+export let stavení = createDeclension(
+  'n',
+  [
+    'stavení',
+    'stavení',
+    'stavení',
+    'stavení',
+    'stavení!',
+    'stavení',
+    'stavením',
+    'stavení',
+    'stavení',
+    'stavením',
+    'stavení',
+    'stavení!',
+    'staveních',
+    'staveními/íma',
+  ],
+  'building',
+  'buildings'
+);
+
+export let kuře = createDeclension(
+  'n',
+  [
+    'kuře',
+    'kuřete',
+    'kuřeti',
+    'kuře',
+    'kuře!',
+    'kuřeti',
+    'kuřetem',
+    'kuřata',
+    'kuřat',
+    'kuřatům',
+    'kuřata',
+    'kuřata!',
+    'kuřatech',
+    'kuřaty/ama',
+  ],
+  'chick',
+  'chicks'
+);
